d9cdc474684ebdf974e93f15e9c46df93a4882dd
TomaszJaworski777 <71073337+TomaszJaworski777@users.noreply.github.com>
2025-08-08 15:30:44 +0200

    Take 2
    Bench: 23050579

--- engine/src/search_engine/mcts/iteration/select_expand.rs
+++ engine/src/search_engine/mcts/iteration/select_expand.rs
@@ -72,7 +72,7 @@ fn get_cpuct(options: &EngineOptions, parent_node: &Node, parent_visits: u32) ->
     let visit_scale = options.cpuct_visit_scale();
     cpuct *= 1.0 + ((f64::from(parent_visits) + visit_scale) / visit_scale).ln();
 
-    let variance = (parent_node.squared_score() - (parent_node.score().single(0.5) as f64).abs()) * options.cpuct_variance_scale();
+    let variance = (parent_node.squared_score() - (parent_node.score().single(0.5) as f64).powi(2)).abs() * options.cpuct_variance_scale();
     cpuct *= 1.0 + options.cpuct_variance_weight() * (variance - 1.0);
 
     cpuct
