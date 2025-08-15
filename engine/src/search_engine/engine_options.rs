3531585a8869385a0e3e5ffef1352eb9c40f5724
TomaszJaworski777 <71073337+TomaszJaworski777@users.noreply.github.com>
2025-08-15 13:08:47 +0200

    Take 1
    Bench: 3196572

--- engine/src/search_engine/engine_options.rs
+++ engine/src/search_engine/engine_options.rs
@@ -30,8 +30,7 @@ create_options! {
             cpuct_visit_scale:     f64  =>  8000.00,  128.0,  65536.0,  800.0,  0.002;
             cpuct_variance_scale:  f64  =>  0.2,      0.1,    50.0,     0.02,   0.002;
             cpuct_variance_weight: f64  =>  0.85,     0.0,    2.0,      0.085,  0.002;
-            cpuct_depth_log:       f64  =>  2.1523,   1.0,    5.0,      0.215,  0.002;
-            cpuct_depth_scale:     f64  =>  6.2415,   1.0,    10.0,     0.624,  0.002;
+            cpuct_depth_scale:     f64  =>  4.7845,   1.0,    10.0,     0.478,  0.002;
             cpuct_min_depth_mul:   f64  =>  0.3517,   0.0,    1.0,      0.035,  0.002;
 
             //Transposition Table
--- engine/src/search_engine/mcts/iteration/select_expand.rs
+++ engine/src/search_engine/mcts/iteration/select_expand.rs
@@ -73,7 +73,7 @@ fn get_cpuct(options: &EngineOptions, parent_node: &Node, depth: f64) -> f64 {
         cpuct *= 1.0 + options.cpuct_variance_weight() * (variance - 1.0);
     }
 
-    cpuct *= (1.0 - depth.log(options.cpuct_depth_log()) / options.cpuct_depth_scale()).max(options.cpuct_min_depth_mul());
+    cpuct *= (1.0 - depth.ln() / options.cpuct_depth_scale()).max(options.cpuct_min_depth_mul());
 
     cpuct
 }
