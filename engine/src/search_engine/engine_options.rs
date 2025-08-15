7608793304317358c71fa79888e669c2db546dc6
Tomasz Jaworski <71073337+TomaszJaworski777@users.noreply.github.com>
2025-08-15 10:49:48 +0200

    Depth cpuct scaling (#7)
    Bench: 2730513

--- engine/src/search_engine/engine_options.rs
+++ engine/src/search_engine/engine_options.rs
@@ -23,11 +23,13 @@ create_options! {
             common_pst: f64  =>  1.00,  0.1,  10.0,  0.1,  0.002;
 
             //Node Selection
-            root_cpuct:            f64  =>  1.15,     0.1,    5.0,      0.115,  0.002;
-            cpuct:                 f64  =>  0.75,     0.1,    5.0,      0.075,  0.002;
+            cpuct:                 f64  =>  1.15,     0.1,    5.0,      0.075,  0.002;
             cpuct_visit_scale:     f64  =>  8000.00,  128.0,  65536.0,  800.0,  0.002;
-            cpuct_variance_scale:  f64  =>  0.2,      0.1,    50.0,     0.02,    0.002;
-            cpuct_variance_weight: f64  =>  0.85,      0.0,    2.0,     0.085,   0.002;
+            cpuct_variance_scale:  f64  =>  0.2,      0.1,    50.0,     0.02,   0.002;
+            cpuct_variance_weight: f64  =>  0.85,     0.0,    2.0,      0.085,  0.002;
+            cpuct_depth_log:       f64  =>  2.1523,   1.0,    5.0,      0.215,  0.002;
+            cpuct_depth_scale:     f64  =>  6.2415,   1.0,    10.0,     0.624,  0.002;
+            cpuct_min_depth_mul:   f64  =>  0.3517,   0.0,    1.0,      0.035,  0.002;
 
             //Transposition Table
             hash_size: f64  =>  0.04,  0.01,  0.5,  0.004,  0.002;
--- engine/src/search_engine/mcts/iteration/select_expand.rs
+++ engine/src/search_engine/mcts/iteration/select_expand.rs
@@ -5,22 +5,20 @@ use crate::{search_engine::engine_options::EngineOptions, Node, SearchEngine, WD
 impl SearchEngine {
     pub(super) fn select_and_expand(&self, position: &mut ChessPosition, selection_stack: &mut Vec<(usize, ZobristKey)>, castle_mask: &[u8; 64]) -> Option<usize> {
         let mut node_idx = self.tree().root_index();
-        let mut root = true;
 
         selection_stack.push((node_idx, position.board().hash()));
 
         loop {
             let parent_node = self.tree().get_node(node_idx);
+            let depth = selection_stack.len() as f64;
 
-            let cpuct = get_cpuct(&self.options(), &parent_node, root);
+            let cpuct = get_cpuct(&self.options(), &parent_node, depth);
 
             node_idx = self.tree().select_child_by_key(node_idx, |child_node| {
                 let score = get_score(&parent_node.score(), child_node, child_node.visits()).single(0.5) as f64;
                 let exploration_factor = f64::from(parent_node.visits().max(1)).sqrt() / f64::from(child_node.visits() + 1);
                 score + cpuct * child_node.policy() * exploration_factor
             }).expect("Failed to select a valid node.");
-
-            root = false;
 
             position.make_move(self.tree().get_node(node_idx).mv(), castle_mask);
 
@@ -63,12 +61,8 @@ fn get_score(parent_score: &WDLScore, child_node: &Node, child_visits: u32) -> W
     score
 }
 
-fn get_cpuct(options: &EngineOptions, parent_node: &Node, root: bool) -> f64 {
-    let mut cpuct = if root {
-        options.root_cpuct()
-    } else {
-        options.cpuct()
-    };
+fn get_cpuct(options: &EngineOptions, parent_node: &Node, depth: f64) -> f64 {
+    let mut cpuct = options.cpuct();
 
     let visit_scale = options.cpuct_visit_scale();
     cpuct *= 1.0 + ((f64::from(parent_node.visits()) + visit_scale) / visit_scale).ln();
@@ -78,6 +72,8 @@ fn get_cpuct(options: &EngineOptions, parent_node: &Node, root: bool) -> f64 {
         let variance = var.sqrt() / options.cpuct_variance_scale();
         cpuct *= 1.0 + options.cpuct_variance_weight() * (variance - 1.0);
     }
+
+    cpuct *= (1.0 - depth.log(options.cpuct_depth_log()) / options.cpuct_depth_scale()).max(options.cpuct_min_depth_mul());
 
     cpuct
 }
