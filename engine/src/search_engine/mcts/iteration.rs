094ff5d3e313c981bc897f39312b5d6cf7d3e359
TomaszJaworski777 <71073337+TomaszJaworski777@users.noreply.github.com>
2025-09-09 14:38:50 +0200

    Fix the crashes (maybe)
    Bench: 3653491

--- engine/src/search_engine/mcts/iteration.rs
+++ engine/src/search_engine/mcts/iteration.rs
@@ -15,27 +15,27 @@ impl SearchEngine {
         castle_mask: &[u8; 64],
     ) -> Option<WDLScore> { 
         let hash = position.board().hash();
-        let node = &self.tree()[node_idx];
+        let node = self.tree()[node_idx].clone();
         let score = if !ROOT && (node.is_terminal() || node.visits() == 0) {
             self.simulate(node_idx, position)
         } else {
             *depth += 1.0;
 
-            if node.children_count() == 0 {
+            if self.tree()[node_idx].children_count() == 0 {
                 if !self.tree().expand_node(node_idx, *depth, position.board(), self.options()) {
                     return None;
                 }
             }
 
-            let new_index = self.select(node_idx, *depth);
+            let new_idx = self.select(node_idx, *depth);
 
-            position.make_move(self.tree()[new_index].mv(), castle_mask);
+            position.make_move(self.tree()[new_idx].mv(), castle_mask);
 
-            self.tree().inc_threads(new_index, 1);
+            self.tree().inc_threads(new_idx, 1);
 
-            let score = self.perform_iteration::<false>(new_index, position, depth, castle_mask);
+            let score = self.perform_iteration::<false>(new_idx, position, depth, castle_mask);
 
-            self.tree().dec_threads(new_index, 1);
+            self.tree().dec_threads(new_idx, 1);
 
             score?
         }.reversed();
