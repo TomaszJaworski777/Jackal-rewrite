7250e3aad0b9db07370fa7715136e47c093ddaf1
TomaszJaworski777 <71073337+TomaszJaworski777@users.noreply.github.com>
2025-09-13 15:40:53 +0200

    Fix display timers
    Bench: 3647066

--- engine/src/search_engine/mcts.rs
+++ engine/src/search_engine/mcts.rs
@@ -17,11 +17,14 @@ impl SearchEngine {
 
         let search_stats = SearchStats::new(0);
 
+        let mut search_report_timer = Instant::now();
+        let mut max_avg_depth = 0;
+
         loop 
         {
             thread::scope(|s| {
                 s.spawn(|| {
-                    self.main_loop::<Display>(&search_stats, &search_limits, &castle_mask);
+                    self.main_loop::<Display>(&search_stats, &search_limits, &castle_mask, &mut search_report_timer, &mut max_avg_depth);
                 });
 
                 for _ in 0..(self.options().threads() - 1) {
@@ -46,17 +49,16 @@ impl SearchEngine {
         search_stats: &SearchStats,
         search_limits: &SearchLimits,
         castle_mask: &[u8; 64],
+        search_report_timer: &mut Instant,
+        max_avg_depth: &mut u64
     ) -> Option<()> {
-        let mut search_report_timer = Instant::now();
-        let mut max_avg_depth = 0;
-
         while !self.is_search_interrupted() {
             self.search_loop(search_stats, search_limits, castle_mask)?;
 
-            if search_stats.avg_depth() > max_avg_depth || search_report_timer.elapsed().as_secs_f64() > 1.0 / Display::refresh_rate_per_second() {
+            if search_stats.avg_depth() > *max_avg_depth || search_report_timer.elapsed().as_secs_f64() > (1.0 / Display::refresh_rate_per_second()) {
                 Display::search_report(search_limits, search_stats, self);
-                search_report_timer = Instant::now();
-                max_avg_depth = max_avg_depth.max(search_stats.avg_depth());
+                *search_report_timer = Instant::now();
+                *max_avg_depth = search_stats.avg_depth().max(*max_avg_depth);
             }
 
             if search_stats.iterations() % 128 != 0 {
