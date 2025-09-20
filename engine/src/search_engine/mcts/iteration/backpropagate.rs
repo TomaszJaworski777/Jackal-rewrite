d46ac3838942f501b3e691d12278d7be7681e20d
Tomasz Jaworski <71073337+TomaszJaworski777@users.noreply.github.com>
2025-09-20 18:43:32 +0200

    Better mate backprop (#24)
    Bench: 3647066

--- engine/src/search_engine/mcts/iteration/backpropagate.rs
+++ engine/src/search_engine/mcts/iteration/backpropagate.rs
@@ -3,35 +3,38 @@ use chess::ZobristKey;
 use crate::{search_engine::tree::{NodeIndex, Tree}, GameState, SearchEngine, WDLScore};
 
 impl SearchEngine {
-    pub(super) fn backpropagate(&self, node_idx: NodeIndex, score: WDLScore, key: ZobristKey) {
+    pub(super) fn backpropagate(&self, node_idx: NodeIndex, child_idx: Option<NodeIndex>, score: WDLScore, key: ZobristKey) {
         self.tree().add_visit(node_idx, score);
-        backprop_state(self.tree(), node_idx);
+        backprop_state(self.tree(), node_idx, child_idx);
         self.tree().hash_table().push(key, score.reversed());
     }
 }
 
-fn backprop_state(tree: &Tree, node_idx: NodeIndex) {
-    let mut proven_loss = true;
-    let mut proven_loss_length = 0;
+fn backprop_state(tree: &Tree, node_idx: NodeIndex, child_idx: Option<NodeIndex>) -> Option<()> {
+    let child_idx = child_idx?;
 
-    if tree[node_idx].children_count() == 0 {
-        return;
+    match tree[child_idx].state() {
+        GameState::Loss(len) => {
+            tree.set_state(node_idx, GameState::Win(len + 1));
+        },
+        GameState::Win(len) => {
+            let mut proven_loss = true;
+            let mut proven_loss_length = len;
+
+            tree[node_idx].map_children(|child_idx| {
+                if let GameState::Win(x) = tree[child_idx].state() {
+                    proven_loss_length = x.max(proven_loss_length);
+                } else {
+                    proven_loss = false;
+                }
+            });
+
+            if proven_loss {
+                tree.set_state(node_idx, GameState::Loss(proven_loss_length + 1));
+            }
+        },
+        _ => (),
     }
 
-    tree[node_idx].map_children(|child_idx| { //TODO: Flip this loop
-        match tree[child_idx].state() {
-            GameState::Loss(len) => {
-                tree.set_state(node_idx, GameState::Win(len + 1));
-                proven_loss = false;
-            },
-            GameState::Win(len) => {
-                proven_loss_length = proven_loss_length.max(len)
-            },
-            _ => proven_loss = false,
-        }
-    });
-
-    if proven_loss {
-        tree.set_state(node_idx, GameState::Loss(proven_loss_length + 1));
-    }
+    Some(())
 }
